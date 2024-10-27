544db7dae4236ef2da5481407576f4f42cb5c276
Abdul Rafay <abdulrafaymehmood123@gmail.com>
2024-10-27 15:51:49 +0000

    made a good progress

--- Tutorial/lessons/rock-paper-scissor/script.js
+++ Tutorial/lessons/rock-paper-scissor/script.js
@@ -6,16 +6,6 @@ function Judge(playerMove) {
     let result = '';
 
     // Generate a new random computer move each time
-    const randomNumber = Math.random();
-    let computerMove = "";
-
-    if (randomNumber >= 0 && randomNumber < 1 / 3) {
-        computerMove = 'rock';
-    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-        computerMove = 'paper';
-    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-        computerMove = 'scissors';
-    }
 
     if (playerMove === computerMove) {
         result = 'Tie';
